5990d516f7488bfedc873de5b186c99614a4132d
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-08-13 11:42:29 -0700

    Set up skeletons for methods

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import Board from './Board'
 
-const Game = () => <div>Game</div>;
+const Game = () => {
+
+    const handleClick = () => {
+
+    
+    }
+
+    const jumpTo = () => {
+        
+    }
+
+
+    return (
+        <Board onClick={handleClick}/>
+    )
+};
 
 export default Game;
