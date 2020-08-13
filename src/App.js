e75110a0bb2eb8e600ccc321a1a5f38561fdaaed
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-08-13 09:23:54 -0700

    Add component skeletons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Game from './components/Game'
 
-const App = () => {
-  return (
-    <div>Tic Tac Toe</div>
-  )
-}
+const App = () => <Game />;
 
 export default App;
