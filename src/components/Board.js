c414c1fa7bb14ab9fa9300fda8a6e40241f20694
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-08-13 11:41:53 -0700

    Add basic styles

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import Square from './Square'
 
-const Board = () => <div>Board</div>;
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+}
+
+const Board = ({ squares, onClick }) => (
+    <div style={ style }>
+        <Square onClick={() => onClick('dummyValue')} value='1' />
+        <Square onClick={() => onClick('dummyValue')} value='2' />
+        <Square onClick={() => onClick('dummyValue')} value='3' />
+        <Square onClick={() => onClick('dummyValue')} value='4' />
+        <Square onClick={() => onClick('dummyValue')} value='5' />
+        <Square onClick={() => onClick('dummyValue')} value='6' />
+        <Square onClick={() => onClick('dummyValue')} value='7' />
+        <Square onClick={() => onClick('dummyValue')} value='8' />
+        <Square onClick={() => onClick('dummyValue')} value='9' />
+    
+    </div>
+);
 
 export default Board;
