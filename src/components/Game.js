a76b9d787139e04bb391378f86b18c37df1b382a
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-08-13 13:10:16 -0700

    Add functionality for MVP

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board'
+import { calculateWinner } from '../helpers';
+
+const styles = {
+    width: '200px',
+    margin: '20px auto',
+}
 
 const Game = () => {
 
-    const handleClick = () => {
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const winner = calculateWinner(board)
 
-    
+    const handleClick = (i) => {
+        const boardCopy = [...board];
+        if (winner || boardCopy[i]) {
+            return;
+        }
+        boardCopy[i] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setXIsNext(!xIsNext);
     }
 
     const jumpTo = () => {
-        
+
+    }
+
+    const renderMoves = () => {
+
     }
 
 
     return (
-        <Board onClick={handleClick}/>
+        <>
+            <Board squares={board} onClick={handleClick}/>
+            <div style={ styles }>
+                <p>
+                    {winner ? `winner: ${winner}` : `Next Player: ${xIsNext ? 'X' : 'O'}`}
+                </p>
+                { renderMoves() }
+            </div>
+        </>
     )
 };
 
